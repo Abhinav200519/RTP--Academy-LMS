5a58b34b33f2afbe5643f02f4ea1e8f1e3d6490c
Nishitha-3 <nishithak0403@gmail.com>
2025-02-05 14:27:55 +0530

    Update Login.php

--- controllers/Login.php
+++ controllers/Login.php
@@ -2,7 +2,6 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
-
     public function __construct()
     {
         parent::__construct();
@@ -13,7 +12,6 @@ class Login extends CI_Controller {
         $this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
         $this->output->set_header('Pragma: no-cache');
     }
-
     public function index() {
         if ($this->session->userdata('admin_login')) {
             redirect(site_url('admin'), '1');
